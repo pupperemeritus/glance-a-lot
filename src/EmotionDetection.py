6628f7bda9db18531e615e17859e1d37a62c20fc
pupper emeritus <pupperemeritus9@protonmail.com>
2023-01-03 00:07:58 +0530

    added ML model for emotion detection

---
+++ src/EmotionDetection.py
@@ -0,0 +1,49 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, classification_report, plot_confusion_matrix
+
+# Reading the data from the dataset
+df = pd.read_csv("4.1LCleaned.csv")
+x = df["text"]
+y = df["emotions"]
+
+cv = CountVectorizer()
+x_vectorized = cv.fit_transform(x.apply(lambda x: np.str_(x)))
+
+pickle.dump(cv, open("vectorizer.pkl", "wb"))
+
+# Splitting the dataframe
+df_split = train_test_split(x_vectorized, y, train_size=0.9, random_state=42)
+
+x_train = df_split[0]
+x_test = df_split[1]
+y_train = df_split[2]
+y_test = df_split[3]
+
+# Fitting to naive bayes model
+nb = MultinomialNB()
+
+nb.fit(x_train, y_train)
+
+accuracyNB = nb.score(x_test, y_test)
+
+# Fitting to naive
+lr = LogisticRegression(max_iter=1500)
+lr.fit(x_train, y_train)
+
+pickle.dump(lr, open("lr.pkl", "wb"))
+
+accuracyLR = lr.score(x_test, y_test)
+
+plot_confusion_matrix(lr, x_test, y_test)
+plt.show()
