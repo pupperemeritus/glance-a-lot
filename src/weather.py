1ff585847fbe1794ac8102e203133604bbdf227b
pupper emeritus <pupperemeritus9@protonmail.com>
2023-01-23 12:35:34 +0530

    completed weather feature

--- src/weather.py
+++ src/weather.py
@@ -1,68 +1,56 @@
 # import required modules
-import requests, json
+import requests
+from datetime import datetime, timezone, timedelta
 
-# Enter your API key here
-api_key = "a1311da525ca1056b2960c9fb7638023"
 
-# base_url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+class OWMWeather:
+    def __init__(self):
+        self.api_key = "a1311da525ca1056b2960c9fb7638023"
+        self.base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# Give city name
-city_name = input("Enter city name : ")
+    def get_weather(self, city_name: str):
+        complete_url = self.base_url + "appid=" + self.api_key + "&q=" + city_name
+        response = requests.get(complete_url)
+        x = response.json()
+        if x["cod"] != "404":
+            y = x["main"]
+            weather_data = {
+                "tempC": y["temp"] - 273,
+                "feels_likeC": y["feels_like"]-273,
+                "temp_minC": y["temp_min"]-273,
+                "temp_maxC": y["temp_max"]-273,
+                "city": x["name"],
+                "country": x["sys"]["country"],
+                "weather": x["weather"][0]["main"],
+                "desc": x["weather"][0]["description"],
+                "humidity": y["humidity"],
+                "cloud_cover": x["clouds"]["all"],
+                "wind_speed": x["wind"]["speed"],
+                "sunrise": x["sys"]["sunrise"],
+                "sunset": x["sys"]["sunset"],
+                "tz": x["timezone"]
+            }
+            return self.data_to_string(weather_data)
 
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+        else:
+            print(" City Not Found ")
 
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
+    @staticmethod
+    def data_to_string(data) -> str:
+        res = f"""Temperature(C): {data["tempC"]}
+Maximum Temperature(C): {data["temp_maxC"]}
+Minimum Temperature(C): {data["temp_minC"]}
+Feels Like(C): {data["feels_likeC"]}
+Humidity (%): {data["humidity"]}
+Description: {data["desc"]}
+Cloud Cover (%): {data["cloud_cover"]}
+Wind Speed (km/h): {data["wind_speed"]}
+Sunrise: {datetime.fromtimestamp(data["sunrise"],tz=timezone(timedelta(hours=5,minutes=30)))}
+Sunset {datetime.fromtimestamp(data["sunset"],tz=timezone(timedelta(hours=5,minutes=30)))}
+City: {data["city"]}, Country: {data["country"]}"""
+        return res
 
-# json method of response object
-# convert json format data into
-# python format data
-x = response.json()
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-if x["cod"] != "404":
-
-	# store the value of "main"
-	# key in variable y
-	y = x["main"]
-
-	# store the value corresponding
-	# to the "temp" key of y
-	current_temperature = y["temp"]
-
-	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
-
-	# store the value corresponding
-	# to the "humidity" key of y
-	current_humidity = y["humidity"]
-
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
-
-	# store the value corresponding
-	# to the "description" key at
-	# the 0th index of z
-	weather_description = z[0]["description"]
-
-	# print following values
-	print(" Temperature (in kelvin unit) = " +
-					str(current_temperature) +
-		"\n atmospheric pressure (in hPa unit) = " +
-					str(current_pressure) +
-		"\n humidity (in percentage) = " +
-					str(current_humidity) +
-		"\n description = " +
-					str(weather_description))
-
-else:
-	print(" City Not Found ")
+if __name__ == "__main__":
+    wthr = OWMWeather()
+    print(wthr.get_weather("Hyderabad"))
